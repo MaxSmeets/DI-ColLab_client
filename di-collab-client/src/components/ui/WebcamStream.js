678da4f85b4aa4a1d9d9bf64cbd8816a172ca643
MaxSmeets <maxsmeets1996@gmail.com>
2024-02-01 11:04:49 +0100

    Actuual webcam screen with overlayed bit sequencer

--- di-collab-client/src/components/ui/WebcamStream.js
+++ di-collab-client/src/components/ui/WebcamStream.js
@@ -2,22 +2,38 @@ import React from "react";
 import { useRoom } from "../../providers/RoomProvider";
 import { useUser } from "../../providers/UserProvider";
 import { FiUser } from "react-icons/fi";
+import AudioMeter from "./AudioMeter";
 
 function WebcamStream({ videoRef }) {
   const { username } = useUser();
-  const { cam } = useRoom();
+  const { cam, mic } = useRoom();
+
   return (
-    <div className='w-screen bg-primary'>
-      <div className='flex flex-col items-center justify-center w-48 h-24 m-2 text-white rounded-md bg-slate-700'>
-        {cam ? (
-          <video className='w-48 h-24 m-2' ref={videoRef} autoPlay />
-        ) : (
-          <div className='flex flex-col items-center justify-center'>
-            {username}
+    <div className='w-screen mx-auto text-white bg-primary'>
+      {cam ? (
+        <div className='relative w-48 h-24 m-2 overflow-hidden rounded-md bg-slate-700'>
+          <video
+            className='absolute top-0 left-0 w-full h-full'
+            ref={videoRef}
+            autoPlay
+          />
+          {mic && (
+            <div className='absolute bottom-0 left-0 w-full'>
+              <AudioMeter />
+            </div>
+          )}
+        </div>
+      ) : (
+        <div className='relative w-48 h-24 m-2 overflow-hidden rounded-md bg-slate-700'>
+          <div className='absolute top-0 left-0 w-full h-full'>
+            {mic && <AudioMeter />}
+          </div>
+          <div className='absolute inset-0 z-10 flex flex-col items-center justify-center'>
+            <div>{username}</div>
             <FiUser size={50} />
           </div>
-        )}
-      </div>
+        </div>
+      )}
     </div>
   );
 }
