842c918e4e027aa42699834adf36a9f42b8ebaf5
MaxSmeets <maxsmeets1996@gmail.com>
2024-02-01 11:01:54 +0100

    Bottom bar to display participating users

--- di-collab-client/src/components/ui/BottomBar.js
+++ di-collab-client/src/components/ui/BottomBar.js
@@ -1,33 +1,48 @@
 import { useRoom } from "../../providers/RoomProvider";
+import { useUser } from "../../providers/UserProvider";
 import React, { useEffect, useRef } from "react";
 import WebcamStream from "./WebcamStream";
+import ExternalWebcamStream from "./ExternalWebcamSteam";
 
 function BottomBar() {
-  const { cam, mic, audio } = useRoom();
+  const { cam, mic, audio, participants } = useRoom();
+  const { volume, setVolume } = useUser();
   const videoRef = useRef(null);
 
   useEffect(() => {
-    if (cam) {
-      navigator.mediaDevices
-        .getUserMedia({ video: true })
-        .then((stream) => {
+    navigator.mediaDevices
+      .getUserMedia({ video: true })
+      .then((stream) => {
+        if (cam) {
           videoRef.current.srcObject = stream;
-        })
-        .catch((error) => {
-          console.error("Error accessing webcam:", error);
-        });
-    }
+        } else {
+          stream.getTracks().forEach((track) => {
+            track.stop();
+            stream.removeTrack(track);
+          });
+        }
+      })
+      .catch((error) => {
+        console.error("Error accessing webcam:", error);
+      });
   }, [cam]);
 
   return (
     <div className='flex flex-row justify-start w-screen overflow-x-hidden'>
       <WebcamStream videoRef={videoRef} />
-      <WebcamStream videoRef={videoRef} />
-      <WebcamStream videoRef={videoRef} />
-      <WebcamStream videoRef={videoRef} />
-      <WebcamStream videoRef={videoRef} />
-      <WebcamStream videoRef={videoRef} />
-      <WebcamStream videoRef={videoRef} />
+      {/* {participants.map((participant) => (
+        <ExternalWebcamStream
+          key={participant.id}
+          participant={participant}
+          videoRef={videoRef}
+        />
+      ))} */}
+      {/* <ExternalWebcamStream externalUsername='JuleanCodes' />
+      <ExternalWebcamStream externalUsername='SanderLienaerts' />
+      <ExternalWebcamStream externalUsername='Brandonnn123' />
+      <ExternalWebcamStream externalUsername='ShiroInoue' />
+      <ExternalWebcamStream externalUsername='Takahiro' />
+      <ExternalWebcamStream externalUsername='Marcelke' /> */}
     </div>
   );
 }
